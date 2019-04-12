0d1987581e4bf70e2e7c740b16b39df00113051a
d0rshBooken <andvr916@student.liu.se>
2019-04-12 14:05:54 +0200

    Broken; Displayed && FUlly drawn not correct

--- pythonParser.py
+++ pythonParser.py
@@ -81,7 +81,7 @@ def parse_line(line, device):
                 device['displayed2'] = (line.split("MainActivity: +")[1])
             else:
                 device['displayed'] = timestamp_to_ms(line.split("MainActivity: +")[1].split("total")[0])
-                device['displayed2'] = (line.split("MainActivity: +")[1].split("total")[0])
+                device['displayed2'] = (line.split("MainActivity: +")[1].split("(total")[0])
                 device['displayed_plus_total'] = timestamp_to_ms(line.split(".MainActivity:")[1].split("total")[1])    
             device['app_name']  = line.split("Displayed ")[1].split("/.MainActivity")[0]
         # Displayed BackdropActivity /!\ Must be else if of previous case 
@@ -96,7 +96,7 @@ def parse_line(line, device):
         # Fully Drawn 
         if re.search("Fully drawn", line):          
             device['fully_drawn'] = timestamp_to_ms(line.split("Fully drawn")[1].split(":")[1])
-                 
+            print(device['fully_drawn'])
 
     # Package installed 
     if re.search("I\/Pm\([0-9]+\)(.*)Package(.*)installed", line) and "android" not in line:          
@@ -109,20 +109,20 @@ def parse_line(line, device):
         # CordovaWebView Started (A)
         if re.search("Apache Cordova native platform", line):
             device['cordova_start'] = float(line.split(":")[2])             
-
+            
         # CordovaWebView Loaded (B): Calculate diff from started untill loaded == B - A 
         if re.search("CordovaWebView is running on device made by", line):
             device['cordova_loaded'] = float(line.split(":")[2])    
-            
+            device['my_deviceready_timing'] =  1000*(device['cordova_loaded'] - device['cordova_start'])
 
     if "INFO:CONSOLE" in line:
         # Ionic Native: deviceready
         if re.search("Ionic Native: deviceready", line):
-            device['deviceready'] = int(line.split("deviceready event fired after")[1].split("ms")[0])       
+            device['deviceready'] = line.split("deviceready event fired after")[1].split("ms")[0]       
             
         # Ionic Loaded 
         if re.search("ionic loaded:", line):
-            device['timer_ionic'] = int(line.split(":")[1].split("ms")[0])              
+            device['timer_ionic'] = line.split(":")[1].split("ms")[0].split('.')[0]     
             
         # Cordova device 
         if re.search("device: Device", line):
@@ -137,21 +137,21 @@ def parse_line(line, device):
         # checkBackendVersionIsActive
         if re.search("checkBackendVersionIsActive", line):
             if not 'timer_backend' in device:
-                device['timer_backend'] = int(line.split("checkBackendVersionIsActive:")[1].split("ms")[0])
+                device['timer_backend'] = int(line.split("checkBackendVersionIsActive:")[1].split("ms")[0].split(".")[0])
                 device['timer_backend_count'] = 1
             else:
                 device['timer_backend_count'] += 1
         # storage.get('loginToken')
         if re.search("get\('loginToken'\)", line):
             if not 'timer_storage' in device:
-                device['timer_storage'] = int(line.split("storage.get('loginToken'):")[1].split("ms")[0])
+                device['timer_storage'] = int(line.split("storage.get('loginToken'):")[1].split("ms")[0].split(".")[0])
                 device['timer_storage_count'] = 1
             else:
                 device['timer_storage_count'] += 1
         # loginService.login()->browser.on('loadstop')
         if re.search("loginService.login", line):
             if not 'timer_loginservice' in device:
-                device['timer_loginservice'] = int(line.split("->browser.on('loadstop'):")[1].split("ms")[0])
+                device['timer_loginservice'] = int(line.split("->browser.on('loadstop'):")[1].split("ms")[0].split(".")[0])
                 device['timer_loginservice_count'] = 1
             else:
                 device['timer_loginservice_count'] += 1
@@ -204,8 +204,8 @@ if __name__ == "__main__":
 
 
      # All exisisting keys in dict
-    csv_columns = ['unique','app_name', 'serial', 'manufacturer', 'platform', 'version', 'cordova',              'model','deviceready','timer_ionic','displayed','displayed2',          'fully_drawn','install_time',                                'timer_backend',                        'timer_storage',                    'timer_loginservice']
-    #csv_columns = ['app_name', 'serial', 'manufacturer', 'platform', 'version', 'cordova', ' source', 'model','deviceready','timer_ionic','displayed','displayed_plus_total','fully_drawn','install_time','cordova_start','cordova_loaded','timer_backend','timer_backend_count','timer_storage','timer_storage_count','timer_loginservice','timer_loginservice_count']
+    csv_columns = ['unique','app_name', 'serial', 'manufacturer', 'platform', 'version', 'cordova',              'model','deviceready', 'my_deviceready_timing','timer_ionic','displayed','displayed2',          'fully_drawn','install_time',                                'timer_backend',                        'timer_storage',                    'timer_loginservice']
+    #csv_columns = ['app_name', 'serial', 'manufacturer', 'platform', 'version', 'cordova', ' source', 'model','deviceready','timer_ionic','displayed','displayed_plus_total','fully_drawn','install_time','cordova_start','cordova_loaded','timer_backend','timer_backend_count','timer_storage','timer_storage_count','timer_loginservice','timer_loginservice_count','cordova_timing']
     
     dict_data = tests
 
